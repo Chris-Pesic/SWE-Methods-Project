d94982b8239c947aa89c157e9f4141517b6ed161
Kieran Gwinnett <40592965@live.napier.ac.uk>
2024-02-08 14:52:22 +0000

    App modified to have basic outline

--- src/main/java/com/napier/GP14/App.java
+++ src/main/java/com/napier/GP14/App.java
@@ -6,4 +6,11 @@ public class App
     {
         System.out.println("Boo yah!");
     }
+
+    //Basic Structure of code
+    //Read in SQl Database into array of objects
+    //Offer selection menu of report types to user, selected via input of a number.
+    //Second menu for level of report requested, eg: country or city
+    //Using Primary Key variables in the various objects fulfill the requested reports parameters
+    //Print the report
 }
