8c451564e5bd46ed302cf7e1c924047eb8ebf619
Kieran Gwinnett <40592965@live.napier.ac.uk>
2024-02-15 13:47:54 +0000

    Began implementation of basic menu system

--- src/main/java/com/napier/GP14/App.java
+++ src/main/java/com/napier/GP14/App.java
@@ -1,10 +1,35 @@
 package com.napier.GP14;
 
+import java.util.Scanner;
+
 public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Boo yah!");
+        //System.out.println("Boo yah!");
+
+        Scanner keyboard = new Scanner(System.in);  // Create a Scanner object
+
+        System.out.println("What report would you like?");
+        System.out.println("A. Country");
+        System.out.println("B. City");
+        System.out.println("C. Capital");
+        System.out.println("D. Population");
+        System.out.println("E. Population of ___");
+        System.out.println("F. Language");
+        System.out.println("G. Complex");
+
+        String selection = keyboard.nextLine();
+
+
+
+        if(selection.equals("A")){
+            //should clear screen to keep things tidy
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            //doesnt appear to work on the intellij "terminal"
+            System.out.println("Country Report");
+        }
     }
 
     //Basic Structure of code
